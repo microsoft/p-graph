e33f0b2215aa4d956c559b28f5db51d7608c9a9c
Christian Gonzalez <1581488+christiango@users.noreply.github.com>
2020-07-10 11:49:12 -0700

    Add new test enforcement

--- src/__tests__/index.tests.ts
+++ src/__tests__/index.tests.ts
@@ -1,48 +1,105 @@
 import pGraph from "../index";
 import { DepGraphArray } from "../types";
 
+interface MockFunctionDefinition {
+  /** A friendly name for the function */
+  name: string;
+
+  /** How many ticks this function should take to simulate the duration of the function execution */
+  duration: number;
+}
+
+/** A record of a function start or end event that can be composed to create an ordered log of function calls **/
+interface MockFunctionCallRecord {
+  /** The name of the function */
+  name: string;
+
+  /** Denotes if this is when the function started or ended execution */
+  state: "start" | "end";
+}
+
+class FunctionScheduler {
+  private currentlyRunningFunctions: { name: string; ticksRemaining: number; resolve: () => void }[] = [];
+
+  private tickScheduled: boolean = false;
+
+  public callRecords: MockFunctionCallRecord[] = [];
+
+  public startExecutingFunction(definition: MockFunctionDefinition): Promise<unknown> {
+    const { name, duration } = definition;
+    this.callRecords.push({ name, state: "start" });
+
+    const promise = new Promise((resolve) => {
+      this.currentlyRunningFunctions.push({ name, ticksRemaining: duration, resolve });
+    });
+
+    this.ensureTickScheduled();
+
+    return promise;
+  }
+
+  private ensureTickScheduled() {
+    if (!this.tickScheduled) {
+      Promise.resolve().then(() => this.tick());
+      this.tickScheduled = true;
+    }
+  }
+
+  private tick() {
+    this.tickScheduled = false;
+    this.currentlyRunningFunctions.forEach((item) => {
+      item.ticksRemaining = item.ticksRemaining - 1;
+    });
+
+    const remainingItems = this.currentlyRunningFunctions.filter((item) => item.ticksRemaining === 0);
+
+    if (remainingItems.length > 0) {
+      remainingItems.forEach((item) => {
+        item.resolve();
+        this.callRecords.push({ name: item.name, state: "end" });
+      });
+      this.ensureTickScheduled();
+    }
+  }
+}
+
+function defineMockFunction(definition: MockFunctionDefinition, functionScheduler: FunctionScheduler): () => Promise<unknown> {
+  return () => functionScheduler.startExecutingFunction(definition);
+}
+
 const gettingDressedTestFunctions = () => {
-  const calls = [];
+  const functionScheduler = new FunctionScheduler();
 
   return {
-    calls,
+    callRecords: functionScheduler.callRecords,
     // Example graph from: https://www.npmjs.com/package/toposort
-    putOnShirt: () =>
-      Promise.resolve("put on your shirt").then((v) => {
-        calls.push(v);
-      }),
-    putOnShorts: () =>
-      Promise.resolve("put on your shorts").then((v) => {
-        calls.push(v);
-      }),
-    putOnJacket: () =>
-      Promise.resolve("put on your jacket").then((v) => {
-        calls.push(v);
-      }),
-    putOnShoes: () =>
-      Promise.resolve("put on your shoes").then((v) => {
-        calls.push(v);
-      }),
-    tieShoes: () =>
-      Promise.resolve("tie your shoes").then((v) => {
-        calls.push(v);
-      }),
+    putOnShirt: defineMockFunction({ name: "putOnShirt", duration: 1 }, functionScheduler),
+    putOnShorts: defineMockFunction({ name: "putOnShorts", duration: 1 }, functionScheduler),
+    putOnJacket: defineMockFunction({ name: "putOnJacket", duration: 1 }, functionScheduler),
+    putOnShoes: defineMockFunction({ name: "putOnShoes", duration: 1 }, functionScheduler),
+    tieShoes: defineMockFunction({ name: "tieShoes", duration: 1 }, functionScheduler),
   };
 };
 
-const ensureValidGettingDressedOrder = (calls: string[]) => {
-  expect(calls.indexOf("tie your shoes")).toBeGreaterThan(calls.indexOf("put on your shoes"));
+const expectScheduledBefore = (callRecords: MockFunctionCallRecord[], firstTaskName: string, secondTaskName: string) => {
+  const firstIndex = callRecords.findIndex((item) => item.name === firstTaskName && item.state === "end");
+  const secondIndex = callRecords.findIndex((item) => item.name === secondTaskName && item.state === "start");
 
-  expect(calls.indexOf("put on your jacket")).toBeGreaterThan(calls.indexOf("put on your shirt"));
+  expect(firstIndex).not.toEqual(-1);
+  expect(secondIndex).not.toEqual(-1);
+  expect(secondIndex).toBeGreaterThan(firstIndex);
+};
 
-  expect(calls.indexOf("put on your jacket")).toBeGreaterThan(calls.indexOf("put on your shorts"));
-
-  expect(calls.indexOf("put on your shoes")).toBeGreaterThan(calls.indexOf("put on your shorts"));
+const ensureValidGettingDressedOrder = (callRecords: MockFunctionCallRecord[]) => {
+  expectScheduledBefore(callRecords, "putOnShoes", "tieShoes");
+  expectScheduledBefore(callRecords, "putOnShirt", "putOnJacket");
+  expectScheduledBefore(callRecords, "putOnShorts", "putOnJacket");
+  expectScheduledBefore(callRecords, "putOnShorts", "putOnShoes");
 };
 
 describe("Public API", () => {
   it("should accept an array dep graph", async () => {
-    const { calls, putOnJacket, putOnShirt, putOnShoes, putOnShorts, tieShoes } = gettingDressedTestFunctions();
+    const { callRecords, putOnJacket, putOnShirt, putOnShoes, putOnShorts, tieShoes } = gettingDressedTestFunctions();
 
     const graph: DepGraphArray = [
       [putOnShoes, tieShoes],
@@ -53,7 +110,7 @@ describe("Public API", () => {
 
     await pGraph(graph).run();
 
-    ensureValidGettingDressedOrder(calls);
+    ensureValidGettingDressedOrder(callRecords);
   });
 
   it("should accept a dependency array with a list of named functions", async () => {
@@ -77,7 +134,7 @@ describe("Public API", () => {
 
     await pGraph(funcs, graph).run();
 
-    ensureValidGettingDressedOrder(testFunctions.calls);
+    ensureValidGettingDressedOrder(testFunctions.callRecords);
   });
 
   it("should accept a dependency map with a list of named functions", async () => {
@@ -102,6 +159,6 @@ describe("Public API", () => {
 
     await pGraph(funcs, depMap).run();
 
-    ensureValidGettingDressedOrder(testFunctions.calls);
+    ensureValidGettingDressedOrder(testFunctions.callRecords);
   });
 });
