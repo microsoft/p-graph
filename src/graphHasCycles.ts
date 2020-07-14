4e3196f9a11a52653dcf27a326d4a982944269b7
Christian Gonzalez <1581488+christiango@users.noreply.github.com>
2020-07-14 12:59:51 -0700

    No recursion on cycle calculation

--- src/graphHasCycles.ts
+++ src/graphHasCycles.ts
@@ -4,34 +4,22 @@ import { PGraphNodeWithDependencies } from "./types";
  * Checks for any cycles in the dependency graph, returning false if no cycles were detected.
  */
 export function graphHasCycles(pGraphDependencyMap: Map<string, PGraphNodeWithDependencies>, nodesWithNoDependencies: string[]): boolean {
-  const checkForCyclesInternal = (currentNodeId: string, visitedNodes: Set<string>): boolean => {
+  const stack: { nodeId: string; visitedNodes: Set<string> }[] = [];
+  nodesWithNoDependencies.forEach((root) => stack.push({ nodeId: root, visitedNodes: new Set() }));
+
+  while (stack.length > 0) {
+    const { nodeId, visitedNodes } = stack.pop()!;
+
     // If we have already seen this node, we've found a cycle
-    if (visitedNodes.has(currentNodeId)) {
+    if (visitedNodes.has(nodeId)) {
       return true;
     }
 
-    visitedNodes.add(currentNodeId);
+    visitedNodes.add(nodeId);
 
-    const node = pGraphDependencyMap.get(currentNodeId)!;
+    const node = pGraphDependencyMap.get(nodeId)!;
 
-    // If we got to a leaf node, this particular path does not have a cycle
-    if (node.dependedOnBy.size === 0) {
-      return false;
-    }
-
-    for (const childId of node.dependedOnBy.keys()) {
-      if (checkForCyclesInternal(childId, new Set(visitedNodes))) {
-        return true;
-      }
-    }
-
-    return false;
-  };
-
-  for (const root of nodesWithNoDependencies) {
-    if (checkForCyclesInternal(root, new Set())) {
-      return true;
-    }
+    [...node.dependedOnBy.keys()].forEach((childId) => stack.push({ nodeId: childId, visitedNodes: new Set(visitedNodes) }));
   }
 
   return false;
